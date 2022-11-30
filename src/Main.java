e07448e34201c9351bcf80421725d15113e12b49
Kaisei <e225721@ie.u-ryukyu.ac.jp>
2022-11-30 21:11:35 +0900

    1st commit

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,7 @@
 public class Main {
     public static void main(String[] args){
         String str = null;
-        
-    
-    try {
         System.out.println(str.length());
-    } catch(NullPointerException e){
-       
-        System.out.println("NullPointerExceptionが発生しました");
-        System.out.println(e.getMessage());
-    }
+    
     }
 }
